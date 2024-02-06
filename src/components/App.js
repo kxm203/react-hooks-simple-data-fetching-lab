f544ca8c22eee8b31af56ba316a0283978e228d1
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-02-05 19:18:17 -0500

    Doggiesgit add .git add .

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dogImage, setDogImage] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(result => {
+                setDogImage(result.message);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            });
+    },[]);
+    return (
+        <div>
+            {loading ? ( 
+                <p>Loading...</p>
+            ) : (
+                <img src={dogImage} alt="A Random Dog" />
+            )}
+        </div>
+    );
+}
+export default App;
